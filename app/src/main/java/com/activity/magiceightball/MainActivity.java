b73bf2906f331469278f98203b007df0c4e639ed
gabo8090 <g-araujo94@hotmail.com>
2019-05-10 08:43:50 -0300

    Incompleto II.

--- app/src/main/java/com/activity/magiceightball/MainActivity.java
+++ app/src/main/java/com/activity/magiceightball/MainActivity.java
@@ -2,6 +2,7 @@ package com.activity.magiceightball;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ImageView ballDisplay = findViewById(R.id.image_eightBall);
+
     }
 }
